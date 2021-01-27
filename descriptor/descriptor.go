matheusd/protobuf-hex-display

multi: create lightninglabs/protobuf pkg

descriptor/descriptor.go
@@ -42,8 +42,8 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/golang/protobuf/proto"
-	protobuf "github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"github.com/lightninglabs/protobuf-hex-display/proto"
+	protobuf "github.com/lightninglabs/protobuf-hex-display/protoc-gen-go/descriptor"
 )
 
 // extractFile extracts a FileDescriptorProto from a gzip'd buffer.

lightninglabs/protobuf

descriptor/descriptor.go
@@ -16,12 +16,12 @@ import (
 	"io/ioutil"
 	"sync"
 
-	"github.com/golang/protobuf/proto"
+	"github.com/lightninglabs/protobuf-hex-display/proto"
 	"google.golang.org/protobuf/reflect/protodesc"
 	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/runtime/protoimpl"
 
-	descriptorpb "github.com/golang/protobuf/protoc-gen-go/descriptor"
+	descriptorpb "github.com/lightninglabs/protobuf-hex-display/protoc-gen-go/descriptor"
 )
 
 // Message is proto.Message with a method to return its descriptor.